72890601c0e53c415ed9e78a05031878df45a7dd
tangwaichung <167169307+tangwaichung@users.noreply.github.com>
2024-04-16 14:46:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.c304ekc86fzg.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "tangwaichung-employee"
+customregion = "us-east-1"
+
